c984ec4532d1d7393c852eb8426cf290cb92e209
Mateo Dominguez <matedom22@gmail.com>
2021-11-05 13:44:43 -0300

    Tarea 2
    Solucion tarea 2

---
+++ tarea2.js
@@ -0,0 +1,41 @@
+//TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
+// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
+// también vamos a crear un <h1> que diga Bienvenido!
+// vas a crear un botón de acción que una vez que lo apretás, va a
+// mostrar toda la información junta en un campo de texto
+// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
+
+/*
+Ejemplo form:
+  <form id="entrada-bar" onsubmit="return false;">
+    <input type="text" placeholder="Ingresá tu nombre" id="nombre-usuario"/>
+    <input type="number" placeholder="Ingresá tu edad" id="edad-usuario" />
+    <input type="submit" value="Ingresar" id="ingresar"/>
+  </form>
+
+  <div id="resultado">Hola</div>
+*
+* */
+
+
+
+
+const botonEnviar = document.querySelector('#boton-enviar');
+botonEnviar.onclick = function(){
+  const nombreUsuario = document.querySelector('#nombre-usuario').value;
+  const edadUsuario = Number(document.querySelector('#edad-usuario').value);
+  const apellidoUsuario = document.querySelector('#apellido-usuario').value;
+ document.querySelector('#resultado').innerText = devolverInformacion(nombreUsuario, apellidoUsuario, edadUsuario);
+ document.querySelector('h1').innerText = saludarUsuario(nombreUsuario);
+
+return false;
+}
+
+function devolverInformacion (nombre, apellido, edad){
+
+return `Tu nombre es ${nombre} ${apellido} y tienes ${edad} anios` 
+
+} 
+function saludarUsuario (nombre){
+  return `Bienvenido ${nombre}`;
+}
